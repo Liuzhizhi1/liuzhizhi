10a13c69d4e974169445beb3687d2785cd7b5656
Liuzhizhi1 <51734495+Liuzhizhi1@users.noreply.github.com>
2019-06-12 17:36:20 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,46 +1,4 @@
 // sw.js
-var cacheName = 'bs-0-2-0';
-var cacheFiles = [
-    '/',
-    './index.html',
-    './index.js',
-    './style.css',
-    './img/book.png',
-    './img/loading.svg'
-];
-
-// 监听install事件，安装完成后，进行文件缓存
-self.addEventListener('install', function (e) {
-    console.log('Service Worker 状态： install');
-    var cacheOpenPromise = caches.open(cacheName).then(function (cache) {
-        return cache.addAll(cacheFiles)> -1;
-    });
-    e.waitUntil(cacheOpenPromise);
-});
-self.addEventListener('fetch', function (e) {
-    // 如果有cache则直接返回，否则通过fetch请求
-    e.respondWith(
-        caches.match(e.request).then(function (cache) {
-            return cache || fetch(e.request);
-        }).catch(function (err) {
-            console.log(err);
-            return fetch(e.request)> -1;
-        })
-    );
-});
-// 监听activate事件，激活后通过cache的key来判断是否更新cache中的静态资源
-self.addEventListener('activate', function (e) {
-    console.log('Service Worker 状态： activate');
-    var cachePromise = caches.keys().then(function (keys) {
-        return Promise.all(keys.map(function (key) {
-            if (key !== cacheName) {
-                return caches.delete(key);
-            }
-        }));
-    })
-    e.waitUntil(cachePromise);
-    return self.clients.claim();
-});
 var apiCacheName = 'api-0-1-1';
 self.addEventListener('fetch', function (e) {
     // 需要缓存的xhr请求
@@ -51,7 +9,7 @@ self.addEventListener('fetch', function (e) {
 
     // 判断当前请求是否需要缓存
     var needCache = cacheRequestUrls.some(function (url) {
-        return e.request.url.indexOf(url);
+        return e.request.url.indexOf(url) > -1;
     });
 
     /**** 这里是对XHR数据缓存的相关操作 ****/
@@ -82,3 +40,42 @@ self.addEventListener('fetch', function (e) {
     }
 });
 
+function getApiDataFromCache(url) {
+    if ('caches' in window) {
+        return caches.match(url).then(function (cache) {
+            if (!cache) {
+                return;
+            }
+            return cache.json();
+        });
+    }
+    else {
+        return Promise.resolve();
+    }
+}
+
+function queryBook() {
+    // ……
+    // 远程请求
+    var remotePromise = getApiDataRemote(url);
+    var cacheData;
+    // 首先使用缓存数据渲染
+    getApiDataFromCache(url).then(function (data) {
+        if (data) {
+            loading(false);
+            input.blur();            
+            fillList(data.books);
+            document.querySelector('#js-thanks').style = 'display: block';
+        }
+        cacheData = data || {};
+        return remotePromise;
+    }).then(function (data) {
+        if (JSON.stringify(data) !== JSON.stringify(cacheData)) {
+            loading(false);                
+            input.blur();
+            fillList(data.books);
+            document.querySelector('#js-thanks').style = 'display: block';
+        }
+    });
+    // ……
+}
